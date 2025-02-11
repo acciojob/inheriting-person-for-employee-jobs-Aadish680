6499c1c90a420b39c94a0eb07fd8936cea31d869
Aadish Shibu <144556449+Aadish680@users.noreply.github.com>
2025-02-11 10:47:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 // complete this js code
-function Person(name, age) {}
 
-function Employee(name, age, jobTitle) {}
+class Person {
+    constructor(name, age) {
+        this.n = name;
+        this.a = age;
+    }
+
+    greet() {
+        return `Hello, my name is ${this.n}, I am ${this.a} years old`;
+    }
+}
+
+class Employee extends Person {
+    constructor(name, age, jobTitle) {
+        super(name, age); 
+        this.job = jobTitle;
+    }
+
+    jobGreet() {
+        return`Hello, my name is ${this.n}, I am ${this.a} years old, and my job title is ${this.job}`;
+    }
+}
+
+
 
 // Do not change code below this line
 window.Person = Person;
